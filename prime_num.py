f664c346e6b93513a6d590ada05dac2d420dccc0
bekmirzayev <147894054+bekmirzayev@users.noreply.github.com>
2023-10-14 10:47:44 +0500

    Create prime_num.py

---
+++ prime_num.py
@@ -0,0 +1,15 @@
+num = int(input())
+
+flag = False
+
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            flag = True
+            break
+    if flag:
+        print("NO")
+    else:
+        print("YES")
