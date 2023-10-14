793fea54cf6138eb1fa0890c773a73ffd43ee29b
bekmirzayev <147894054+bekmirzayev@users.noreply.github.com>
2023-10-14 09:27:59 +0500

    Create def_sum.py

---
+++ def_sum.py
@@ -0,0 +1,12 @@
+
+def summa(*sonlar):
+    """Kiritilgan sonlar yig'indisini hisoblaydigan funksiya"""
+    yigindi = 0
+    for son in sonlar:
+        yigindi += son
+    return yigindi
+
+
+print(summa(1, 2))
+print(summa(1, 2, 3, 4, 5))
+print(summa(4, 5, 6, 7))
