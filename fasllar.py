2993d1db9483a768f39b5dcddcd0f3af30208b5f
bekmirzayev <147894054+bekmirzayev@users.noreply.github.com>
2023-10-14 10:04:41 +0500

    Create fasllar.py

---
+++ fasllar.py
@@ -0,0 +1,11 @@
+t=int(input())
+if t==12 or t==1 or t==2:
+    print("Winter")
+elif t==3 or t==4 or t==5:
+    print("Spring")
+elif t==6 or t==7 or t==8:
+    print("Summer")
+elif t==9 or t==10 or t==11:
+    print("Autumn")
+else:
+    print("Error")
