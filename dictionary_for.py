aa6f6404746585fe6ca3e84ad888a9f30d6eedc5
bekmirzayev <147894054+bekmirzayev@users.noreply.github.com>
2023-10-14 09:05:05 +0500

    Create dictionary_for.py

---
+++ dictionary_for.py
@@ -0,0 +1,10 @@
+python_words = {
+    "integer": "Butun son",
+    "float": "O'nlik son",
+    "boolean": "Mantiqiy qiymat",
+    "for": "Biror amalni qayta-qayta bajarish tsikli",
+    "if": "Shartlarni tekshirish operatori",
+}
+
+for key, value in sorted(python_words.items()):
+    print(f"{key.title()} - {value}")
