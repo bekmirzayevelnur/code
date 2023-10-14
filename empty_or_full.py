cf0ddbb34d42cfa72409fa7eb257c51bebc4b1dd
bekmirzayev <147894054+bekmirzayev@users.noreply.github.com>
2023-10-14 10:54:02 +0500

    Create empty_or_full.py

---
+++ empty_or_full.py
@@ -0,0 +1,10 @@
+# # Funktsiya bool()belgilangan ob'ektning mantiqiy qiymatini qaytaradi.
+
+# Ob'ekt har doim "True" ni qaytaradi, agar:
+
+# Ob'ekt bo'sh, masalan, [], (), {}
+# Ob'ekt noto'g'ri
+# Ob'ekt 0
+# Ob'ekt Yo'q
+x = bool()
+print(x)
