6a335a24b0a4f4d643c1c5b96847c9d04914c505
bekmirzayev <147894054+bekmirzayev@users.noreply.github.com>
2023-10-14 11:08:33 +0500

    Create taqqosla.py

---
+++ taqqosla.py
@@ -0,0 +1,17 @@
+# Python program to demonstrate the 
+# use of cmp() method
+
+# when a<b
+a = 1
+b = 2
+print(cmp(a, b)) 
+
+# when a = b 
+a = 2
+b = 2
+print(cmp(a, b)) 
+
+# when a>b 
+a = 3
+b = 2
+print(cmp(a, b))
