a1b09f0150897606853a3d3e2e13a496f1e9ec6e
bekmirzayev <147894054+bekmirzayev@users.noreply.github.com>
2023-10-14 08:52:47 +0500

    even_or_odd.py

---
+++ even_or_odd.py
@@ -0,0 +1,4 @@
+juft_sonlar = list(range(0,20,2)) # 0 dan 20 gacha 2 qadam bilan
+toq_sonlar = list(range(1,20,2))  # 1 dan 20 gacha 2 qadam bilan
+print("Juft sonlar: ", juft_sonlar)
+print("Toq sonlar: ", toq_sonlar)
