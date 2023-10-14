54846d24a12b06366c4b68854ed190bd4ca67480
bekmirzayev <147894054+bekmirzayev@users.noreply.github.com>
2023-10-14 09:14:44 +0500

    Create while1.py

---
+++ while1.py
@@ -0,0 +1,8 @@
+savol = "Sevgan kitobingizni kiriting"
+savol += "(barcha kitoblarni kiritib bo'lgach 'exit' deb yozing): "
+
+while True:
+    kitob = input(savol)
+    if kitob == "exit":
+        break
+print("Rahmat!")
