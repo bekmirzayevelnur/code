cea763678615c213e437909e27493e35b8095b9b
bekmirzayev <147894054+bekmirzayev@users.noreply.github.com>
2023-10-14 09:19:35 +0500

    Create def.py

---
+++ def.py
@@ -0,0 +1,10 @@
+def toliq_ism_yasa(ism, familiya):
+    """Toliq ism qaytaruvchi funksiya"""
+    toliq_ism = f"{ism} {familiya}"
+    return toliq_ism
+
+
+talaba1 = toliq_ism_yasa("olim", "hakimov")
+talaba2 = toliq_ism_yasa("hakim", "olimov")
+print(f"Darsga kelmagan talabalar: {talaba1} va {talaba2}")
+print(f"{talaba1} darsga kechikib keldi")
