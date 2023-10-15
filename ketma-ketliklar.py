342331c0f59d371aead68fdfbde9c1b3d1f44ef5
bekmirzayev <147894054+bekmirzayev@users.noreply.github.com>
2023-10-15 12:08:01 +0500

    Create ketma-ketliklar.py

---
+++ ketma-ketliklar.py
@@ -0,0 +1,13 @@
+# ushbu ketma-ketlik formulasi	1, 2, 4, 7, 11, 16, 22, 29, 37, 46, 56, 67, 79, 92, 106, 121
+n=int(input())
+print((n-1)*((n-1)+1)//2 + 1)
+////////////////////////////////////////
+# ushbu ketma-ketlik formulasi  1, 8, 22, 43, 71
+n=int(input())
+print((n-1)*((n-1)+1)//2 + 1)
+//////////////////////////////////////
+# ushbu ketma-ketlik formulasi  0, 7, 26, 63, 124, 215
+a=int(input())
+print(a**3-1)
+/////////////////////////////////////
+# ushbu ketma-ketlik formulasi  
