2066dee949cdf8f49350bd4a275f2346141bdc4e
bekmirzayev <147894054+bekmirzayev@users.noreply.github.com>
2023-10-15 11:27:56 +0500

    Create otkan_kunlar.py

---
+++ otkan_kunlar.py
@@ -0,0 +1,7 @@
+n=input()
+a=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+b=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+for i in a:
+    print(i,n.count(i))
+for i in b:
+    print(i,n.count(i))
