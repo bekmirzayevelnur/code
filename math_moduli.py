e177dd2e4b1b557ef81b31237ba2c9e10ab55e69
bekmirzayev <147894054+bekmirzayev@users.noreply.github.com>
2023-10-15 15:19:07 +0500

    Create math_moduli.py

---
+++ math_moduli.py
@@ -0,0 +1,31 @@
+math.acos()    # arc cosinus qiymati
+
+math.asin()    #arc sinus qiymati
+
+math.atan()    # arc tangens qiymati
+
+math.ceil()  #Raqamni eng yaqin butun songacha yaxlitlaydi
+
+math.cos()     # cosinus qiymati
+
+math.dist()     #Ikki nuqta (p va q) orasidagi Evklid masofasini qaytaradi, bu erda p va q bu nuqtaning koordinatalar
+
+math.exp()     #X darajasiga ko'tarilgan E ni qaytaradi
+
+math.fabs()       #absolyut qiymati
+
+math.fmod()        #qoldiqni qaytaradi
+
+import math
+a=[1,2,3,45,6,7,8]
+print(math.fsum(a))        #listni yigindisi
+
+math.gcd()       #2 sonning EKUBini qaytaradi
+
+math.lcm()          #2 sonning EKUKini qaytaradi
+
+math.isqrt()      #Kvadrat ildiz sonini pastga qarab eng yaqin butun songa yaxlitlaydi
+
+math.pow()          #darajagaga kotaradi
+
+math.sqrt()      #ildizdan chiqaradi
