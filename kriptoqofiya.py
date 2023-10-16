abbdd7e999c94f2d81b806382704645de5e46960
bekmirzayev <147894054+bekmirzayev@users.noreply.github.com>
2023-10-16 18:49:08 +0500

    Create kriptoqofiya.py

---
+++ kriptoqofiya.py
@@ -0,0 +1,22 @@
+s = input()
+
+if s == 's':
+    print(5)
+elif s == 'i':
+    print(8)
+elif s == 'n':
+    print(7)
+elif s == 'u':
+    print(2)
+elif s == 'k':
+    print(3)
+elif s == 'o':
+    print(9)
+elif s == 't':
+    print(4)
+elif s == 'a':
+    print(0)
+elif s == 'g':
+    print(6)
+elif s == 'e':
+    print(1)
