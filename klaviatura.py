83c44b4558afcac1bd73ad25da9358fcc3bd2c3c
py <147894054+bekmirzayev@users.noreply.github.com>
2023-10-25 09:06:01 +0500

    Create klaviatura.py

---
+++ klaviatura.py
@@ -0,0 +1,4 @@
+a=input()
+dic={"q":"m w","w":"q e","e":"w r","r":"e t","t":"r y","y":"t u","u":"y i","i":"u o","o":"i p","p":"o a","a":"p s","s":"a d","d":"s f","f":"d g","g":"f h","h":"g j","j":"h k","k":"j l","l":"k z",
+    "z":"l x","x":"z c","c":"x v","v":"c b","b":"v n","n":"b m","m":"n q"}
+print(dic[a])
