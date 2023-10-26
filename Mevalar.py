3edd6d606cf3172e9b79321a5f2f112a09d7ddd8
py <147894054+bekmirzayev@users.noreply.github.com>
2023-10-26 22:14:10 +0500

    Create Mevalar.py

---
+++ Mevalar.py
@@ -0,0 +1,16 @@
+s,t = map(int,input().split())
+a,b = map(int,input().split())
+m,n = map(int,input().split())
+d = list(map(int,input().split()))
+d1 = list(map(int,input().split()))
+x = 0
+y = 0
+for i in d:
+    if i >= (s-a) and i <= (t-a):
+        x+=1
+for j in d1:
+    if j <= (t - b) and j >= (s - b):
+        y += 1
+
+print(x)
+print(y)
