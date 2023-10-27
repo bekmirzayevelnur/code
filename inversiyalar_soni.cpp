33577b11fc20e765989cb565031bc760f3b45efa
py <147894054+bekmirzayev@users.noreply.github.com>
2023-10-27 08:11:47 +0500

    Create inversiyalar_soni.cpp

---
+++ inversiyalar_soni.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int _mergeSort(int arr[], int temp[],
+               int left, int right);
+int merge(int arr[], int temp[], int left,
+          int mid, int right);
+
+int mergeSort(int arr[], int array_size)
+{
+    int temp[array_size];
+    return _mergeSort(arr, temp, 0, array_size - 1);
+}
+
+int _mergeSort(int arr[], int temp[],
+               int left, int right)
+{
+    int mid, inv_count = 0;
+    if (right > left) {
+
+        mid = (right + left) / 2;
+
+        inv_count += _mergeSort(arr, temp,
+                                left, mid);
+        inv_count += _mergeSort(arr, temp,
+                                mid + 1, right);
+
+        inv_count += merge(arr, temp, left,
+                           mid + 1, right);
+    }
+    return inv_count;
+}
+
+int merge(int arr[], int temp[], int left,
+          int mid, int right)
+{
+    int i, j, k;
+    int inv_count = 0;
+
+    i = left;
+    j = mid;
+    k = left;
+    while ((i <= mid - 1) && (j <= right)) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        }
+        else {
+            temp[k++] = arr[j++];
+
+            inv_count = inv_count + (mid - i);
+        }
+    }
+
+    while (i <= mid - 1)
+        temp[k++] = arr[i++];
+
+    while (j <= right)
+        temp[k++] = arr[j++];
+
+    for (i = left; i <= right; i++)
+        arr[i] = temp[i];
+
+    return inv_count;
+}
+
+int n, a[100009];
+main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i=0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int ans = mergeSort(a, n);
+
+    cout << ans << '\n';
+}
